9bff444b0cb695608a50c092424e9060f7d79e27
Mathias Mahnke <ap27@opennet-initiative.de>
2017-12-24 11:06:08 +0100

    fix cron.d entry

--- debian/cron.d
+++ debian/cron.d
@@ -5,4 +5,4 @@
 MAILTO=root
 
 # export new Opennet Blog entries to Opennet twitter
-*/10 *	* * *	on-twitterbot	/usr/share/on-twitterbot/twitterbot.py rss >/dev/null
+*/10 *	* * *	on-twitterbot	/usr/share/on-twitterbot/on-twitterbot.py rss >/dev/null
